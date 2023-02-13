5befe5f12e536dfb4233202176dff12ee35b4da4
Miledi75 <mileto.di.marco@howest.be>
2023-02-13 20:24:37 +0100

    Add project files.

---
+++ Pri.Ca.Infrastructure/Data/ApplicationDbcontext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pri.Ca.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pri.Ca.Infrastructure.Data
+{
+    public class ApplicationDbcontext : DbContext 
+    {
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+        public ApplicationDbcontext(DbContextOptions<ApplicationDbcontext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
